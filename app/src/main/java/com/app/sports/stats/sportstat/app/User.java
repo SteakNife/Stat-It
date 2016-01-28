feff060b024816316f1f06483ba8f1773813745e
Padraic Cashin <padraic.cashin@gmail.com>
2016-01-28 21:13:27 +0000

    adding new users class and modified AndroidManifest so i can connect to server

---
+++ app/src/main/java/com/app/sports/stats/sportstat/app/User.java
@@ -0,0 +1,27 @@
+package com.app.sports.stats.sportstat.app;
+
+/**
+ * Created by Padraic on 26/01/2016.
+ */
+public class User {
+
+    private String name, email, password, confirmPassword;
+
+    public User(final String name, final String email, final String password, final String confirmPassword){
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+
+    }
+
+    public User (final String email, final String password){
+
+        this.name = "";
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = "";
+
+    }
+}
