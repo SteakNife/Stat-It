723f484cad30574bba83df184208020c661ea539
padraic.cashin@gmail.com <cashman0>
2016-03-08 15:11:44 +0000

    Adding Service Classes.

---
+++ app/src/main/java/com/app/sports/stats/sportstat/app/service/Player/PlayerService.java
@@ -0,0 +1,7 @@
+package com.app.sports.stats.sportstat.app.service.Player;
+
+/**
+ * Created by emoibmo on 08/03/2016.
+ */
+public class PlayerService {
+}
---
+++ app/src/main/java/com/app/sports/stats/sportstat/app/service/Stat/StatService.java
@@ -0,0 +1,7 @@
+package com.app.sports.stats.sportstat.app.service.Stat;
+
+/**
+ * Created by emoibmo on 08/03/2016.
+ */
+public class StatService {
+}
