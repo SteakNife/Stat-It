7b2bda5cccfb70d3fe248f199f8c08ea3d79b225
padraic.cashin@gmail.com <cashman0>
2016-03-08 15:07:20 +0000

    Added UserService.

---
+++ app/src/main/java/com/app/sports/stats/sportstat/app/service/User/UserService.java
@@ -0,0 +1,17 @@
+package com.app.sports.stats.sportstat.app.service.User;
+
+import com.app.sports.stats.sportstat.app.backendlessObjects.User;
+
+/**
+ * Created by emoibmo on 08/03/2016.
+ */
+public class UserService {
+
+    public int authenticateUser (String username, String password) {
+        return 0;
+    }
+
+    public int registerUser (User user) {
+        return 0;
+    }
+}
